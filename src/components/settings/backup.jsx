3d3478c3f54abb91ee30f61283be55205ab7227f
defiver <defiver@proton.me>
2024-08-13 21:01:01 +0300

    fix

---
+++ src/components/settings/backup.jsx
@@ -0,0 +1,77 @@
+import { Button, Flex, Popconfirm } from "antd";
+import { useRef } from "react";
+import { useDB } from "@/hooks/useDB";
+import { importInto, exportDB } from "dexie-export-import";
+import test from './test.json';
+
+export default function Backup({ db }) {
+	const inputRef = useRef();
+
+	const [clearData, isClearDataLoading] = useDB(async () => {
+		await db.tables.forEach((table) => db[table.name].clear());
+		await db.delete({ disableAutoOpen: false });
+	}, false);
+
+	const saveData = async (blob) => {
+		const link = document.createElement('a');
+		link.href = URL.createObjectURL(blob);
+		link.download = "portfolio.json";
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	}
+
+	const uploadData = (e) => {
+		const fileReader = new FileReader();
+		fileReader.readAsText(e.target.files[0], "UTF-8");
+		fileReader.onload = (e) => {
+			importData(JSON.parse(e.target.result));
+		};
+	};
+
+	const [importData, isImportDataLoading] = useDB(async (data) => {
+		const bytes = new TextEncoder().encode(JSON.stringify(data));
+		const blob = new Blob([bytes], { type: "application/json;charset=utf-8" });
+		await clearData();
+		await importInto(db, blob);
+	}, false);
+
+	const [exportData, isExportDataLoading] = useDB(async () => {
+		const blob = await exportDB(db);
+		await saveData(blob)
+	}, false);
+
+	const disable = isImportDataLoading || isExportDataLoading || isClearDataLoading;
+
+	return (
+		<Flex vertical gap={16}>
+			<Button
+				onClick={exportData}
+				loading={isExportDataLoading}
+				disabled={disable}
+			>Export data</Button>
+
+			<Button
+				onClick={() => inputRef.current.click()}
+				loading={isImportDataLoading}
+				disabled={disable}
+			>Import data</Button>
+			<input onChange={uploadData} multiple={false} ref={inputRef} type="file" hidden />
+
+			<Button
+				onClick={() => importData(test)}
+				loading={isImportDataLoading}
+				disabled={disable}
+			>Import test data</Button>
+
+			<Popconfirm
+				title="Delete all data?"
+				onConfirm={() => clearData(clearData)}
+				okText="Yes"
+				cancelText="No"
+			>
+				<Button loading={isClearDataLoading} disabled={disable}>Clear data</Button>
+			</Popconfirm>
+		</Flex>
+	);
+}
--- src/components/settings/index.jsx
+++ src/components/settings/index.jsx
@@ -1,80 +1,28 @@
-import { SettingOutlined } from "@ant-design/icons";
-import { FloatButton, Modal, Button, Flex, Popconfirm } from "antd";
-import { useState, useRef } from "react";
-import { useDB } from "@/hooks/useDB";
-import { importInto, exportDB } from "dexie-export-import";
-import test from './test.json';
+import { DownloadOutlined } from "@ant-design/icons";
+import { FloatButton, Modal } from "antd";
+import { useState } from "react";
+import Backup from './backup';
 
 export default function Settings({ db }) {
   const [swowModal, setSwowModal] = useState(false);
-  const inputRef = useRef();
-
-  const [clearData, isClearDataLoading] = useDB(async () => {
-    await db.tables.forEach((table) => db[table.name].clear());
-  }, false);
-
-  const saveData = async (blob) => {
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = "portfolio.json";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
-
-  const uploadData = (e) => {
-    const fileReader = new FileReader();
-    fileReader.readAsText(e.target.files[0], "UTF-8");
-    fileReader.onload = (e) => {
-      importData(JSON.parse(e.target.result));
-    };
-  };
-
-  const [importData, isImportDataLoading] = useDB(async (data) => {
-    const bytes = new TextEncoder().encode(JSON.stringify(data));
-    const blob = new Blob([bytes], { type: "application/json;charset=utf-8" });
-    await clearData();
-    await importInto(db, blob);
-  }, false);
-
-  const [exportData, isExportDataLoading] = useDB(async () => {
-    const blob = await exportDB(db);
-    await saveData(blob)
-  }, false);
 
   return (
     <>
       <FloatButton
         onClick={() => setSwowModal(true)}
         style={{ insetInlineStart: 24, insetBlockEnd: 72 }}
-        icon={<SettingOutlined />}
+        icon={<DownloadOutlined />}
       />
 
       <Modal
-        title="Settings"
+        title="Backup"
         footer={null}
         open={swowModal}
         onOk={() => setSwowModal(false)}
         onCancel={() => setSwowModal(false)}
         width={300}
       >
-        <Flex vertical gap={16}>
-          <Button onClick={() => importData(test)} loading={isImportDataLoading}>Import test data</Button>
-
-          <Button onClick={exportData} loading={isExportDataLoading}>Export data</Button>
-
-          <Button onClick={() => inputRef.current.click()}>Import data</Button>
-          <input onChange={uploadData} multiple={false} ref={inputRef} type="file" hidden />
-
-          <Popconfirm
-            title="Delete all data?"
-            onConfirm={() => clearData(clearData)}
-            okText="Yes"
-            cancelText="No"
-          >
-            <Button loading={isClearDataLoading}>Clear data</Button>
-          </Popconfirm>
-        </Flex>
+        <Backup db={db} />
       </Modal >
     </>
   );
