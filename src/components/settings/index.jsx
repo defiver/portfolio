4a9022e887fb9e835c93978b2d657eefe8cd2ee1
defiver <defiver@proton.me>
2024-10-09 21:45:54 +0300

    fix

--- src/components/settings/index.jsx
+++ src/components/settings/index.jsx
@@ -1,7 +1,7 @@
 import { DownloadOutlined } from "@ant-design/icons";
 import { Button, Modal } from "antd";
 import { useState } from "react";
-import Backup from './backup';
+import Backup from './Backup';
 
 export default function Settings({ db }) {
   const [swowModal, setSwowModal] = useState(false);
