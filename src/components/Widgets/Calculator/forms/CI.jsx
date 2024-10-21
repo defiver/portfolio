35960ceccb660a269c2dcfb1d0d9dffe5f7f5fd4
defiver <defiver@proton.me>
2024-10-21 20:31:17 +0300

    added compound interest calculator

--- src/components/Widgets/Calculator/forms/CI.jsx
+++ src/components/Widgets/Calculator/forms/CI.jsx
@@ -1,5 +1,5 @@
 import { ClearOutlined } from "@ant-design/icons";
-import { Row, Col, Button, Table, Form, Card, Select } from "antd";
+import { Row, Col, Button, Table, Form, Card, Select, Statistic } from "antd";
 import { localeNumber } from "@/utils/number";
 import { useEffect, useState } from "react";
 import MyInputNumber from "@/components/MyInputNumber";
@@ -8,8 +8,8 @@ const columns = [
 	{ title: 'Год', dataIndex: 'key', key: 'key' },
 	{ title: 'Начальная сумма', dataIndex: 'begin', key: 'begin', render: (t) => localeNumber(t), },
 	{ title: 'Пополнения', dataIndex: 'refill', key: 'refill', render: (t) => localeNumber(t), },
-	{ title: 'Конечная сумма', key: 'end', dataIndex: 'end', render: (t) => localeNumber(t), },
 	{ title: 'Доход', dataIndex: 'income', key: 'income', render: (t) => localeNumber(t), },
+	{ title: 'Конечная сумма', key: 'end', dataIndex: 'end', render: (t) => localeNumber(t), align: "right", },
 ];
 
 export default function CI({ showDesc }) {
@@ -19,23 +19,50 @@ export default function CI({ showDesc }) {
 
 	const calc = () => {
 		const years = [];
+		var capital = values.startingCapital;
 		var prevCapital = values.startingCapital;
-		var reinvestment = (365 / values.reinvestment);
-		var refill = values.refillFrequency > 0 ? values.refill * 365 / values.refillFrequency : 0;
-		var profitability = values.profitability / 100;
+		var dayProfitability = values.profitability / 36500; // ежедневная доходность
+		var days = 365 * values.period; // кол-во дней инвестирования
+		var yearRefill = values.refill * parseInt(365 / values.refillFrequency); // сумма пополнений за год
+		var profit = 0; // накопленные проценты
 
-		for (let i = 0; i < values.period; i++) {
-			let capital = (prevCapital + refill) * (1 + profitability / reinvestment) ** (reinvestment);
+		for (let day = 1; day <= days; day++) {
+			profit += capital * dayProfitability;
 
+			// если наступил срок реинвестирвоания процентов
+			if (day % values.reinvestment === 0) {
+				capital += profit;
+				profit = 0;
+			}
+
+			// если наступил срок пополнения
+			if (values.refillFrequency > 0 && day % values.refillFrequency === 0) {
+				capital += values.refill;
+			}
+
+			// если прошёл год
+			if (day % 365 === 0) {
+				years.push({
+					key: years.length + 1,
+					begin: prevCapital,
+					income: Math.round(capital - prevCapital - yearRefill),
+					refill: yearRefill,
+					end: Math.round(capital),
+				});
+
+				prevCapital = Math.round(capital);
+			}
+		}
+
+		// проверка на случай, если период дробный
+		if (years.length < values.period) {
 			years.push({
-				key: i + 1,
+				key: years.length + 1,
 				begin: prevCapital,
-				income: capital - prevCapital - refill,
-				refill: refill,
-				end: capital,
+				income: Math.round(capital - prevCapital),
+				refill: yearRefill * (values.period - years.length),
+				end: Math.round(capital),
 			});
-
-			prevCapital = capital;
 		}
 
 		setResult(years);
@@ -43,11 +70,12 @@ export default function CI({ showDesc }) {
 
 	const options = [
 		{ label: "Каждый год", value: 365 },
-		{ label: "Каждые полгода", value: 365 / 2 },
-		{ label: "Каждый квартал", value: 365 / 4 },
-		{ label: "Каждый месяц", value: 365 / 12 },
+		{ label: "Каждые полгода", value: 182 },
+		{ label: "Каждый квартал", value: 91 },
+		{ label: "Каждый месяц", value: 30 },
 		{ label: "Каждую неделю", value: 7 },
 		{ label: "Каждый день", value: 1 },
+		{ label: "Никогда", value: 0 },
 	]
 
 	useEffect(() => {
@@ -80,13 +108,13 @@ export default function CI({ showDesc }) {
 						</Form.Item>
 					</Col>
 					<Col span={8}>
-						<Form.Item label="Начисление процентов" name="reinvestment" rules={[{ required: true, message: "" }]}>
+						<Form.Item label="Реинвестирование %" name="reinvestment" rules={[{ required: true, message: "" }]}>
 							<Select options={options} />
 						</Form.Item>
 					</Col>
 					<Col span={8}>
 						<Form.Item label="Частота пополнений" name="refillFrequency" rules={[{ required: true, message: "" }]}>
-							<Select options={[...options, { label: "Никогда", value: 0 }]} />
+							<Select options={options} />
 						</Form.Item>
 					</Col>
 					<Col span={6}>
@@ -99,6 +127,16 @@ export default function CI({ showDesc }) {
 					</Col>
 				</Row>
 			</Form>
+
+			<Row gutter={[8, 0]} style={{ textAlign: "center" }}>
+				<Col span={8}>
+					<Statistic title="Начальная сумма" value={result.length ? localeNumber(result[0].begin) : 0} />
+				</Col>
+				<Col span={8}>
+					<Statistic title="Пополнения" value={result.length ? localeNumber(result.map(o => o.refill).reduce((a, b) => a + b)) : 0} />
+				</Col>
+				<Col span={8}><Statistic title="Итоговая сумма" value={result.length ? localeNumber(result.at(-1).end) : 0} /></Col>
+			</Row>
 
 			<Table bordered size={"small"} columns={columns} dataSource={result} pagination={false} />
 		</>
