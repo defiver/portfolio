6d21c9fa899c12c6db0429b28610cbbc5fa8e9a5
defiver <defiver@proton.me>
2025-01-19 10:48:16 +0300

    added links on tags

--- src/components/Journal/position/Label.jsx
+++ src/components/Journal/position/Label.jsx
@@ -28,18 +28,30 @@ export default function Label({ db, position }) {
     label.push(<Tag title="Доход" key="income" className={position?.income < 0 ? "warning-b" : "good-b"}>{income}</Tag>);
   }
 
+  // оборачиваем элемент в ссылку
+  const wrapWithFirstLink = (links, Children) => {
+    return links && links.match(/^https?:\/\/([^/?#\s]+)/i)
+      ? <a key={Children?.key} href={links.match(/^([^\s]+)/i)[1]} onClick={e => e.stopPropagation()}>{Children}</a>
+      : Children;
+  }
+
   // добавляем сеть
-  chain && label.push(<Typography.Text key="chain" code style={stringToBackground(chain)}>{chain}</Typography.Text>);
+  chain && label.push(wrapWithFirstLink(position.transactions, <Typography.Text key={"chain"} code style={stringToBackground(chain)}>{chain}</Typography.Text>));
 
   // добавляем теги
-  if (Array.isArray(position.tags)) {
-    label = label.concat(position.tags.slice(0, Math.max(0, 4 - label.length)).map((tag) => <Typography.Text code key={tag} style={stringToBackground(tag)}>{tag}</Typography.Text>));
+  if (Array.isArray(position.tags) && position.tags.length > 0) {
+    let first = position.tags[0];
+    label.push(wrapWithFirstLink(position.links, <Typography.Text key={first} code style={stringToBackground(first)}>{first}</Typography.Text>));
+
+    label = label.concat(position.tags.slice(1, Math.max(0, 4 - label.length)).map((tag) => <Typography.Text code key={tag} style={stringToBackground(tag)}>{tag}</Typography.Text>));
   }
 
   return (
     <Row gutter={[48, 8]}>
       <Col span={12} className={"ant-col-grid"}>
-        <Typography.Paragraph ellipsis={true} style={{ marginBottom: 0 }}>{position.text}</Typography.Paragraph>
+        <Typography.Paragraph ellipsis={true} style={{ marginBottom: 0 }}>
+          {position.text && position.text.split("\n")[0]}
+        </Typography.Paragraph>
       </Col>
 
       <Col span={12} style={{ textAlign: "end", overflow: "hidden", height: 24 }}>
