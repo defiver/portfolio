f11f1e696564faf1273675113e1379b87fc34e41
defiver <defiver@proton.me>
2025-03-31 13:39:40 +0300

    fixed api link

--- src/components/Widgets/Merkl/Opportunity.jsx
+++ src/components/Widgets/Merkl/Opportunity.jsx
@@ -4,7 +4,7 @@ import { useLiveQuery } from "dexie-react-hooks";
 import OppList from './OppList';
 import chainIds from './chainIds.json';
 
-const LINK = "https://api.merkl.xyz/v3/opportunity?campaigns=false&testTokens=false";
+const LINK = "https://api.merkl.xyz/v4/opportunities/?items=5000&status=LIVE&test=false";
 
 export default function Opportunity({ db }) {
   const allOpp = useLiveQuery(() => db.merkl.toArray(), [], []);
@@ -14,13 +14,13 @@ export default function Opportunity({ db }) {
     const opportunities = await fetchingGet(LINK);
     if (Object.keys(opportunities).length > 0) {
       const live = Object.values(opportunities)
-        .filter(o => o.status === "live")
+        .filter(o => o.status === "LIVE")
         .map(o => {
-          let fresh = !allOpp.some(m => o.id === m.id);
+          let fresh = !allOpp.some(m => o.identifier === m.id);
           let chainId = chainIds[o.chainId] !== undefined ? o.chainId : "0";
           let chainName = chainIds[chainId].name;
-          let url = `https://merkl.angle.money/${chainName}/${o.action}/${o.id.replace("_", "/")}`;
-          return { id: o.id, name: o.name, url, apr: o.apr, fresh, chain: chainId };
+          let url = `https://app.merkl.xyz/opportunities/${chainName}/${o.type}/${o.identifier}`;
+          return { id: o.identifier, name: o.name, url, apr: o.aprRecord.cumulated, fresh, chain: chainId };
         });
       await db.transaction('rw', db.merkl, async function () {
         await db.merkl.clear();
